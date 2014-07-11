d3a5211149650c1bed4bcabe9f67e046bee05667
brian626 <brianeck@gmail.com>
2014-07-11 14:34:57 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brainstorm::Application.config.secret_key_base = 'd9b90913849ec1c397f631a4b2c38d07cd8447148ce2569a304e112fed8b58818c593fa4bd6f6515fa3460a34f307f9168a7e74d7e9e464d7d0896c2519ccff0'
